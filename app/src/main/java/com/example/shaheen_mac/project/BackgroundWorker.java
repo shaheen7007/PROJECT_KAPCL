bc2199cf4f0255ea94c80770873f40f250247a17
shaheen-mac <shaheenbsv@gmail.com>
2017-07-21 10:28:33 +0530

    Rejected commit: Default

--- app/src/main/java/com/example/shaheen_mac/project/BackgroundWorker.java
+++ app/src/main/java/com/example/shaheen_mac/project/BackgroundWorker.java
@@ -1,11 +1,19 @@
 package com.example.shaheen_mac.project;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,6 +26,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by ADMIN on 9/28/2016.
@@ -25,7 +36,10 @@ import java.net.URLEncoder;
 public class BackgroundWorker extends AsyncTask<String,Void,String> {
     Context context;
     AlertDialog alertDialog;
+     ProgressDialog pd;
     String result="";
+    sqlite mydb;
+    String user_name,oid;
     Context c1;
     BackgroundWorker(Context ctx)
     {
@@ -37,17 +51,26 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
 
 
 
-        String login_url="http://kapcl.net16.net/wc/signin.php";
+        String login_url="http://kapcl.esy.es/wc/signin.php";
 
         //change url
-        String reg_url="http://kapcl.net16.net/wc/signup.php";
+        String reg_url="http://kapcl.esy.es/wc/signup.php";
+
+        String checkout_url="http://kapcl.esy.es/wc/buy.php";
+        String myorder_url="http://kapcl.esy.es/wc/purchaselog.php";
+        String cancel_url="http://kapcl.esy.es/wc/cancelorders.php";
 
         if(type.equals("login"))
         {
 
             try {
 
-                    String user_name=params[1];
+
+//             pd = ProgressDialog.show(context,
+  //                  "", "Connecting..", true);
+
+
+                user_name=params[1];
                     String password=params[2];
                 URL url = new URL(login_url);
                 HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
@@ -88,6 +111,107 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
 
 
         }
+
+
+        else if(type.equals("myorders"))
+        {
+
+            try {
+
+                user_name=params[1];
+                //String password=params[2];
+                URL url = new URL(myorder_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data=URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String line="";
+                while((line=bufferedReader.readLine())!=null)
+                {
+                    result+=line;
+
+                }
+
+
+
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+            }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        else if(type.equals("cancelorder"))
+        {
+
+            try {
+
+                user_name=params[1];
+                oid=params[2];
+                //String password=params[2];
+                URL url = new URL(cancel_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data=URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"+URLEncoder.encode("order_id","UTF-8")+"="+URLEncoder.encode(oid,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String line="";
+                while((line=bufferedReader.readLine())!=null)
+                {
+                    result+=line;
+
+                }
+
+
+
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+            }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+
+
+
         else if (type.equals("register"))
         {
 
@@ -145,6 +269,60 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
 
         }
 
+        else if (type.equals("checkout"))
+        {
+
+
+            try {
+
+                String jsonarr=params[1];
+                String phone=params[2];
+                //String pin=params[3];
+              //  String mail=params[4];
+                //String phone=params[5];
+                //String pass=params[6];
+
+                URL url = new URL(checkout_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data=URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(phone,"UTF-8")+"&"+URLEncoder.encode("items","UTF-8")+"="+URLEncoder.encode(jsonarr,"UTF-8");
+
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="",line="";
+                while((line=bufferedReader.readLine())!=null)
+                {
+                    result+=line;
+
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+            }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+
+        }
+
+
 
         return null;
 
@@ -156,22 +334,161 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
         alertDialog=new AlertDialog.Builder(context).create();
         alertDialog.setTitle("LOGIN STATUS");
 
+//          pd = ProgressDialog.show(context,
+  //           "", "Connecting..", true);
+
 
     }
 
     @Override
     protected void onPostExecute(String result) {
-        if (result.equals("Login success"))
+
+        String togetMyOrders,forcncel;
+
+        togetMyOrders= result.substring(0, 7);
+        forcncel= result.substring(0, 5);
+//        pd.dismiss();
+        String trmdTogetMO=togetMyOrders.trim();
+
+
+
+        String trimdresult=result.trim();
+       //Toast.makeText(context,trimdresult,Toast.LENGTH_SHORT).show();
+
+        if (trimdresult.equals("Login success"))
         {
-            final Intent intent=new Intent(context,ShopActivity.class);
-            context.startActivity(intent);
-            ((MainActivity)context).finish();
+
+           // Toast.makeText(context,"sddsfdffdds",Toast.LENGTH_SHORT).show();
+
+            mydb=new sqlite(context);
+
+            boolean check=mydb.USERInsertdata(user_name);
+
+            if (check==true) {
+                final Intent intent = new Intent(context, dispctivity.class);
+                context.startActivity(intent);
+                ((MainActivity) context).finish();
+               // pd.dismiss();
+            }
         }
 
-        alertDialog.setMessage(result);
+        if (trimdresult.equals("Signup success"))
+        {
 
-        Log.i(result, "onPostExecute: ");
-        alertDialog.show();
+            Toast.makeText(context,"Signup success\nPlease go back and Login",Toast.LENGTH_SHORT).show();
+
+        }
+        if (trimdresult.equals("Account Not Verified"))
+        {
+
+            Toast.makeText(context,"Account Not Verified",Toast.LENGTH_SHORT).show();
+
+        }
+
+
+
+
+        if (trimdresult.equals("Incorrect username or password"))
+        {
+
+
+
+            final AlertDialog.Builder alertDialog= new AlertDialog.Builder(context);
+            alertDialog.setTitle("Incorrect phone number or password");
+            alertDialog.setMessage("Note that password is case sensitive");
+
+           // final Intent intent = new Intent(context, MainActivity.class);
+            //context.startActivity(intent);
+            //((MainActivity) context).finish();
+
+            alertDialog.setNegativeButton("OK",new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO Auto-generated method stub
+
+                    dialog.dismiss();
+                   //pd.dismiss();
+
+                }
+            });
+
+            alertDialog.show();
+
+
+
+
+
+        }
+
+
+        if (trimdresult.equals("cancelled"))
+        {
+
+            Toast.makeText(context,"Your order has been cancelled",Toast.LENGTH_SHORT).show();
+
+
+            // if (check==true) {
+                final Intent intent = new Intent(context, dispctivity.class);
+                context.startActivity(intent);
+                ((MyordersActivity) context).finish();
+            //}
+        }
+
+
+        if (trmdTogetMO.equals("{\"logs\""))
+        {
+
+               // final Intent intent = new Intent(context, MyordersActivity.class);
+            Intent i = new Intent(context, MyordersActivity.class);
+            i.putExtra("res",result);
+
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+
+        if (trmdTogetMO.equals("stock"))
+        {
+
+
+            Intent i = new Intent(context, dispctivity.class);
+            //i.putExtra("res",result);
+
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+
+            Toast.makeText(context,"Order Cancelled",Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+        }
+
+
+
+
+        //checkout success
+       else if (trimdresult.equals("checkout success"))
+        {
+
+           // Toast.makeText(context,"sds",Toast.LENGTH_LONG).show();
+            Intent i = new Intent(context, ConformActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+         //   pd.dismiss();
+
+
+        }
+
+
+     //  Toast.makeText(context,result,Toast.LENGTH_LONG).show();
+
+    //   alertDialog.setMessage(result);
+
+       // Log.i(result, "onPostExecute: ");
+        // alertDialog.show();
 
     }
 
